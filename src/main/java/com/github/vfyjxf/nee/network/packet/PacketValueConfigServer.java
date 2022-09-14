f06c687f2ea63c5eb2607b7a8caf693ff4ec3bca
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-09-14 02:24:03 +0200

    spotless zzzzz

--- src/main/java/com/github/vfyjxf/nee/network/packet/PacketValueConfigServer.java
+++ src/main/java/com/github/vfyjxf/nee/network/packet/PacketValueConfigServer.java
@@ -8,7 +8,6 @@ import appeng.container.slot.SlotRestrictedInput;
 import com.github.vfyjxf.nee.block.tile.TilePatternInterface;
 import com.github.vfyjxf.nee.container.ContainerPatternInterface;
 import com.github.vfyjxf.nee.network.NEENetworkHandler;
-import com.github.vfyjxf.nee.utils.GuiUtils;
 import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
