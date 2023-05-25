203ae6c5f9f51f64bad3c0c9a54f4a2f8de9984c
Raven Szewczyk <git@eigenraven.me>
2023-05-25 19:10:29 +0100

    Use RFG Tags instead of token replacement

--- src/main/java/com/github/vfyjxf/nee/NotEnoughEnergistics.java
+++ src/main/java/com/github/vfyjxf/nee/NotEnoughEnergistics.java
@@ -32,7 +32,7 @@ public class NotEnoughEnergistics {
 
     public static final String MODID = "neenergistics";
     public static final String NAME = "NotEnoughEnergistics";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
+    public static final String VERSION = Tags.VERSION;
     public static final String DEPENDENCIES = "required-after:NotEnoughItems;required-after:appliedenergistics2@[rv3.beta.22,),[rv3.beta-22,),[rv3-beta.22,),[rv3-beta-22,);after:ae2wct;after:ae2fc";
     public static final String GUI_FACTORY = "com.github.vfyjxf.nee.config.NEEConfigGuiFactory";
     public static final Logger logger = LogManager.getLogger("NotEnoughEnergistics");
